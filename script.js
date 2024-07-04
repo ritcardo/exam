df667e5c902903f118d7f421e3908a21cb6338d3
Ricardo <rbotones24@gmail.com>
2024-07-04 16:22:49 +0800

    Revert "Added navigation bar to index.html, styled it in style.css, and added click event  in script.js"
    This reverts commit 83d82e3c36458eccdf01a3adb86a1975dad0cf1b.

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const navItems = document.querySelectorAll('.navbar ul li a');
 
-    navItems.forEach(item => {
-        item.addEventListener('click', function(event) {
-            event.preventDefault();
-            alert(`Clicked on ${item.textContent}`);
-            // You can add more functionality here based on your requirements
-        });
-    });
-});
+function greet() {
+    console.log("Hello, world!");
+}
