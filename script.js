277910b2a4192e13fa241052fb63a240d728c566
Ricardo <rbotones24@gmail.com>
2024-07-04 16:36:41 +0800

    push my files in Github

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
-function greet() {
-    console.log("Hello, world!");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const navItems = document.querySelectorAll('.navbar ul li a');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            alert(`Clicked on ${item.textContent}`);
+
+        });
+    });
+});
